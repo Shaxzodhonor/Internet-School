819792598f757737c5a6f719314224237da3e44c
Shaxzodhonor <mastermusic2529@gmail.com>
2023-09-12 07:53:58 +0500

    commit

--- src/pages/Statistics/index.jsx
+++ src/pages/Statistics/index.jsx
@@ -1,56 +1,86 @@
-import { useEffect, useState } from "react";
-import request from "../../request";
+import { useEffect, useState } from 'react';
+import SunEditor from 'suneditor-react';
+import request from '../../request';
+
 
 const Statistics = () => {
-  const [all, setAll] = useState()
+  const [editor, setEditor] = useState();
 
-  useEffect(() => {
-    request.get("/government").then(data => {
-      if (data.status === 200) {
+  useEffect(()=>{
+    request.get(`/statistic`).then(data => {
+      if(data?.status === 200 && data?.data?.data?.length) {
+        setEditor(data?.data?.data[0])
         console.log(data?.data?.data);
-        setAll(data?.data.data)
       }
     })
   },[])
 
-  function SubmitForm (evt) {
-    evt.preventDefault();
-    // const form = evt.target.elements;
-    // const obj = [...Array(4)].reduce((acc, _, id) => {    
-    //   return form[id]?.name === "file" ? {...acc, file: {id: form[id]?.value}} : {...acc, [form[id]?.name]: form[id]?.value}
-    // },{})
-    request.post("/government", {name: "Salom", file: {id: 19}, link: "salom.uz"}).then(data => {      
+  function EditForm(evt) {
+    evt.preventDefault();   
+    
+    request.patch("/statistic", editor).then(data => {      
       if(data.status === 200) {
-        console.log(data?.data);
-        setAll((prevState) => ([data?.data?.data, ...prevState]))
         alert("Success")
+        setEditor(data?.data?.data)
       }
     })
     .catch((err) => {
       alert(err?.message)
       console.log(err);
     })
-  }
+  
+  }  
 
   return (
     <>
-      <div className="row">
-        <div className="col-7">
-          <form onSubmit={SubmitForm} className='border border-1 border-dark bg-white rounded p-5'>
-            <div className="form-floating mb-3">
-              <input type="text" className="form-control"/>
-              <label>Statistasadaika qiymati</label>
+      <div className="row h-100">
+        <div className="col-12">
+          <form onSubmit={EditForm} className='border border-1 border-dark bg-white rounded p-5'>       
+          <div className='row'>
+              <div className='col'>
+                <div className="mb-3">
+                  <label htmlFor="statistc1" className="form-label">Statistika - 1</label>
+                  <input type="text" className="form-control" id="statistc1" value={editor?.title1 || ""} onChange={(e) => setEditor(prSt => ({...prSt, title1: e?.target?.value}))}/>
+                </div>
+              </div>
+              <div className="col">
+                <div className="mb-3">
+                  <label htmlFor="statistic_1" className="form-label">Statistila - 1</label>
+                  <input type="text" className="form-control" id="statistic_1" value={editor?.number1 || ""} onChange={(e) => setEditor(prSt => ({...prSt, number1: e?.target?.value}))}/>
+                </div>
+              </div>
             </div>
-            <div className="form-floating">
-              <input type="text" name='title' className="form-control"/>
-              <label>Statistika matni</label>
+            <div className='row'>
+              <div className='col'>
+                <div className="mb-3">
+                  <label htmlFor="statistic2" className="form-label">Statistika - 2</label>
+                  <input type="text" className="form-control" id="statistic2" value={editor?.title2 || ""} onChange={(e) => setEditor(prSt => ({...prSt, title2: e?.target?.value}))}/>
+                </div>
+              </div>
+              <div className="col">
+                <div className="mb-3">
+                  <label htmlFor="statistic_2" className="form-label">Statistika - 2</label>
+                  <input type="text" className="form-control" id="statistic_2" value={editor?.number2 || ""} onChange={(e) => setEditor(prSt => ({...prSt, number2: e?.target?.value}))}/>
+                </div>
+              </div>
             </div>
+            <div className='row'>
+              <div className='col'>
+                <div className="mb-3">
+                  <label htmlFor="statistic3" className="form-label">Statistika - 3</label>
+                  <input type="text" className="form-control" id="statistic3" value={editor?.title3 || ""} onChange={(e) => setEditor(prSt => ({...prSt, title3: e?.target?.value}))}/>
+                </div>
+              </div>
+              <div className="col">
+                <div className="mb-3">
+                  <label htmlFor="statistic_3" className="form-label">Statistika - 3</label>
+                  <input type="text" className="form-control" id="statistic_3" value={editor?.number3 || ""} onChange={(e) => setEditor(prSt => ({...prSt, number3: e?.target?.value}))}/>
+                </div>
+              </div>
+            </div>  
             <button type='submit' className='btn btn-outline-dark mt-3'>Submit</button>
           </form>
-        </div>
-        <div className="col-5">
-          <div className='h-100 border border-1 border-dark bg-white rounded p-5'></div>
-        </div>
+        </div>      
       </div>
     </>
   );
